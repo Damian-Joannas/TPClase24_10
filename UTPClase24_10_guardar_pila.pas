581eadebd3348d85101f237de894fcd2d973bb45
Matias Kloster <100063391+KlosterMatias@users.noreply.github.com>
2023-10-24 16:57:04 -0300

    1.5

---
+++ UTPClase24_10_guardar_pila.pas
@@ -0,0 +1,36 @@
+
+Unit UTPClase24_10_guardar_pila;
+
+Interface
+
+Uses crt, UTPClase24_10_definion, UTPClase24_10_procpila;
+{-------------------------------------------------------------------------}
+
+Implementation
+Procedure proccargar_elem(matriz:t_matriz; Var pila:t_pila);
+
+Var 
+  i,j: Integer;
+  aux: Real;
+Begin
+  For i:=1 To fil Do
+    For j:=1 To col Do
+      Begin
+        aux := matriz[i,j].electricidad+matriz[i,j].gas+matriz[i,j].guardias+matriz[i,j].lavanderia+matriz[i,j].conserjeria;
+        APILAR(pila,aux);
+      End;
+End;
+
+{-------------------------------------------------------------------------}
+Procedure procmostrar_pila(Var pila:t_pila);
+
+Var 
+  i: Integer;
+  aux: Real;
+Begin
+  For i:=(fil*Col) Downto 1 Do
+    DESAPILAR(pila,aux);
+  Write('el departamento ',i,'tiene q pagar ', aux:0:3);
+End;
+
+End.
